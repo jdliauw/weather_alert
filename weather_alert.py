5dc5b0657a4dd0c677db0098e04410c90542e71b
Jon Liauw <jdliauw@gmail.com>
2016-07-11 23:22:19 -0400

    This version correctly scans from Google Calendar, and parses the AccuWeather forecast HTML for rain percentage and sends a text message

--- weather_alert.py
+++ weather_alert.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from apiclient import discovery
+from bs4 import BeautifulSoup
 from datetime import date, timedelta
 from oauth2client import client
 from oauth2client import tools
@@ -11,44 +12,35 @@ import oauth2client
 import os
 import requests
 
-
-
-def get_credentials():
+def get_gcal_credentials():
     home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
+    cal_credential_dir = os.path.join(home_dir, '.credentials')
     scopes = 'https://www.googleapis.com/auth/calendar.readonly'
     client_secret_file = 'client_secret.json'
     application_name = 'Google Calendar API Python Quickstart'
 
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+    if not os.path.exists(cal_credential_dir):
+        os.makedirs(cal_credential_dir)
+    credential_path = os.path.join(cal_credential_dir, 'calendar-python-quickstart.json')
 
     store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
+    cal_credentials = store.get()
 
-    if not credentials or credentials.invalid:
+    if not cal_credentials or cal_credentials.invalid:
         flow = client.flow_from_clientsecrets(client_secret_file, scopes)
         flow.user_agent = application_name
         if flags:
-            credentials = tools.run_flow(flow, store, flags)
+            cal_credentials = tools.run_flow(flow, store, flags)
         else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+            cal_credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
-    return credentials
+    return cal_credentials
 
-def main():
-
-    account_sid = 'ACbf6ce5600928a164340f6f4b086e643d'
-    auth_token = '30c506a6affa75080179ae8227a77321'
-
-    client = TwilioRestClient(account_sid, auth_token)
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
+def get_gcal_events():
+    cal_credentials = get_gcal_credentials()
+    http = cal_credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
-    vcard = True
     day_start = datetime.datetime.utcnow().isoformat() + 'Z'
-    day_end = date.today()
     day_offset = timedelta(days=1)
     day_end = (datetime.datetime.now() + day_offset).isoformat() + 'Z'
 
@@ -57,26 +49,39 @@ def main():
         orderBy='startTime').execute()
     events = eventsResult.get('items', [])
 
+    return events
+
+def main():
+
+    account_sid = 'ACbf6ce5600928a164340f6f4b086e643d'
+    auth_token = '30c506a6affa75080179ae8227a77321'
+    client = TwilioRestClient(account_sid, auth_token)
+    events = get_gcal_events()
+    vcard = True
+
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        
-        if (event['summary'][-2:]) == '-o':
+        tag = event['summary'][-2:]
+
+        if (tag == '-o'):
 
             time = str(event['start'].get('dateTime'))[11:16]
-            hour = int(time[:2])
+            hour = time[:2]
+            url = 'http://www.accuweather.com/en/us/university-fl/32826/hourly-weather-forecast/2274320?hour=' + hour
 
-            url = 'http://www.accuweather.com/en/us/university-fl/32826/hourly-weather-forecast/2274320?hour=' + str(hour)
+            if vcard:
+                url_request = requests.get(url)
+                html = url_request.text
+                soup = BeautifulSoup(html, 'html.parser')
+                rain_percent = (soup.find('div', {'class' : 'data-bar'}).span['style'][8:-2])
+                vcard = False
 
-            # if vcard:
-            #     url_request = requests.get(url)
-            #     html = url_request.text
-            #     soup = BeautifulSoup(html, 'html.parser')
-            #     vcard = False
-
-            # else:
-            #     rain_percent = int(soup.find('div', {'class' : 'data-bar'}).span['style'][8:-2])
+            else:
+                rain_percent = (soup.find('div', {'class' : 'data-bar'}).span['style'][8:-2])
             
-            text = 'You have ' + event['summary'][:-3] + ' scheduled at '
+            text = 'You have \'' + event['summary'][:-3] + '\' scheduled at ' + \
+                    time + ', and there is a ' + rain_percent + ' chance of rain.'
+                
             print (text)
         	# client.messages.create(to = '12078389206', from_ = '12078353209', body = text)
 
